106f7fabf8badc4c3273900e4a7cb7d59ab1d1ba
Gyver <sechan9999@me.com>
2026-01-15 19:13:07 -0500

    Configure GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import AnchorCurrencyDashboard from './components/AnchorCurrencyDashboard'
+import UltimateEconomySim from './components/UltimateEconomySim'
 
 function App() {
-    return <AnchorCurrencyDashboard />
+    return (
+        <div className="min-h-screen bg-slate-900 flex items-center justify-center p-4">
+            <div className="w-full max-w-4xl">
+                <UltimateEconomySim />
+            </div>
+        </div>
+    )
 }
 
 export default App
